9f22d509c085d2b148982f5d2be949ff30a71376
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-18 10:39:59 -0400

    update

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+
+import { Box } from '@mui/system';
+
+import {appWrapper,
+  contentWrapper,
+  sideBarWrapper,
+  contentAreaWrapper,
+  scrollListWrapper,
+  createAccountWrapper,
+  joinRedditWrapper,
+
+} from "@styles/styles"
+
+function App() {
+  return (
+    <Box sx={appWrapper}>
+hi
+
+</Box>
+  );
+}
+
+export default App;
---
+++ front/src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { appBar, toolbarWrapper } from '@styles/styles';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
